b6953a9ba6d2aa9c2cc5075eabb8f8811bb56aa8
Ali Shakirzhanuly <gqwerty595@gmail.com>
2022-08-30 19:14:12 +0600

    Initial commit

---
+++ src/main/java/bot/BetterBetterBotApplication.java
@@ -0,0 +1,13 @@
+package bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterBetterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterBetterBotApplication.class, args);
+    }
+
+}
